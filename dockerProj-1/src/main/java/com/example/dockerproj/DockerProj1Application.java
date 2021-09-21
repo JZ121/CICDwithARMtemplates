82722460a89bdc2ce21201b2b6dd5b3419966d0d
jafar <jafar_jz@hotmail.com>
2021-09-21 16:31:19 +0530

    first commit

---
+++ dockerProj-1/src/main/java/com/example/dockerproj/DockerProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.dockerproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProj1Application.class, args);
+	}
+
+}
